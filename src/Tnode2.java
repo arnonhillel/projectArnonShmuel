8ce42455da30374464283de87bc31c120d81640b
smulic <shmhad123@gmail.com>
2018-07-28 23:30:28 +0300

    Median update

---
+++ src/Tnode2.java
@@ -0,0 +1,39 @@
+public class Tnode2 {
+
+    private Data _data;
+    private TNode _left,_right,_parent;
+
+    public Data get_data() {
+        return _data;
+    }
+
+    public void set_data(Data _data) {
+        this._data = _data;
+    }
+
+    public TNode get_left() {
+        return _left;
+    }
+
+    public void set_left(TNode _left) {
+        this._left = _left;
+    }
+
+    public TNode get_right() {
+        return _right;
+    }
+
+    public void set_right(TNode _right) {
+        this._right = _right;
+    }
+
+    public TNode get_parent() {
+        return _parent;
+    }
+
+    public void set_parent(TNode _parent) {
+        this._parent = _parent;
+    }
+
+
+}
